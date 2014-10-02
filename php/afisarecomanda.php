336ae472b669d7fe5bb465598aa0d094a21e868a
alinadaniela30 <alina.mocan14@gmail.com>
2014-10-02 11:10:53 +0300

    Update afisarecomanda.php

--- php/afisarecomanda.php
+++ php/afisarecomanda.php
@@ -1,9 +1,7 @@
   <?php
 
-	$respObj = array();
 	$respObj["status"] = "OK";
 	$respObj["statusMsg"] = "";
-	$dataObj  = json_decode($_POST['dataObj'], true);
 	
 		// creare conexiune la mysqli server + db
 		$connex=mysqli_connect("localhost","root","MocanA78","comenzi");
@@ -17,28 +15,34 @@
 		$sqlquery="SELECT Nume, Prenume, Strada, Numar, Apt, Oras, Judet, Telefon, Email, Model, Cantitate, Conditii FROM formular ";
 		
 		$result=mysqli_query($connex,$sqlquery);
-		$row=mysqli_fetch_array($result,MYSQLI_NUM);
 		
-//error_log($row[0]);
+		if ($result) {
+			$respObjRows = array();
+			$i=0;
+			while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+				$i = $i + 1;
+				$respObjRow = array();
+				$respObjRow["Nume"] = $row["Nume"];
+				$respObjRow["Prenume"] = $row["Prenume"];
+				$respObjRow["Strada"] = $row["Strada"];
+				$respObjRow["Numar"] = $row["Numar"];
+				$respObjRow["Apt"] = $row["Apt"];
+				$respObjRow["Oras"] = $row["Oras"];
+				$respObjRow["Judet"] = $row["Judet"];
+				$respObjRow["Telefon"] = $row["Telefon"];
+				$respObjRow["Email"] = $row["Email"];
+				$respObjRow["Model"] = $row["Model"];
+				$respObjRow["Cantitate"] = $row["Cantitate"];
+				$respObjRow["Conditii"] = $row["Conditii"];
+				
+				array_push($respObjRows,$respObjRow);
+				
+			}
+			$respObj["data"]=$respObjRows;
+	
+		}		
 		
-		if ($row) {
-			$respObj["Nume"] = $row[0];
-			$respObj["Prenume"] = $row[1];
-			$respObj["Strada"] = $row[2];
-			$respObj["Numar"] = $row[3];
-			$respObj["Apt"] = $row[4];
-			$respObj["Oras"] = $row[5];
-			$respObj["Judet"] = $row[6];
-			$respObj["Telefon"] = $row[7];
-			$respObj["Email"] = $row[8];
-			$respObj["Model"] = $row[9];
-			$respObj["Cantitate"] = $row[10];
-			$respObj["Conditii"] = $row[11];
-			
-			
-			
-		
-		} else {
+		else {
 			$respObj["status"] = "ERROR";
 			$respObj["statusMsg"] = "Owner info not found !";
 		}
